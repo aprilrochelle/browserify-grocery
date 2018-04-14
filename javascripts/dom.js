9b018dd753e16fde1db52c0946f89102919933ee
April Nichols <nicholsar@gmail.com>
2018-04-14 11:58:22 -0500

    mouseover events

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,7 +1,9 @@
+const events = require('./events');
 const outputString = document.getElementById('departments');
 
 const printToDom = (deptArray) => {
   outputString.innerHTML = buildDomString(deptArray);
+  events.addDeptEvents();
 };
 
 const buildDomString = (deptArray) => {
@@ -10,7 +12,7 @@ const buildDomString = (deptArray) => {
   deptArray.forEach((dept) => {
     deptCard += `<div class="col-md-3 department">`;
     deptCard +=   `<img src="${dept.img}">`;
-    deptCard +=   `<h3 class="hide">${dept.name}</h3>`;
+    deptCard +=   `<h3 class="hide department-title data-department-id="${dept.id}">${dept.name}</h3>`;
     deptCard += `</div>`;
   });
   deptCard += `</div></div>`;
---
+++ javascripts/events.js
@@ -0,0 +1,29 @@
+const deptCards = document.getElementsByClassName('department');
+
+const showDeptName = (e) => {
+  const deptImg = e.target.children[0];
+  const deptName = e.target.children[1];
+
+  deptImg.classList.add('grey-out');
+  deptName.classList.remove('hide');
+};
+
+const hideDeptName = (e) => {
+  const deptImg = e.target.children[0];
+  const deptName = e.target.children[1];
+
+  deptImg.classList.remove('grey-out');
+  deptName.classList.add('hide');
+};
+
+const addDeptEvents = () => {
+  for (let i = 0; i < deptCards.length; i++) {
+    deptCards[i].addEventListener('mouseenter', showDeptName);
+    deptCards[i].addEventListener('mouseleave', hideDeptName);
+    // deptCards[i].addEventListener('click', showItems);
+  };
+};
+
+module.exports = {
+  addDeptEvents,
+};
