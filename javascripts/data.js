c1036b1bf7fd50c66e7a3c4d71a3eb31c4b354fd
April Nichols <nicholsar@gmail.com>
2018-04-16 18:48:11 -0500

    get and set items

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,14 +1,25 @@
 let departments = [];
+let items = [];
 
 const getDepts = () => {
   return departments;
+};
+
+const setItems = (itemsArray) => {
+  items = itemsArray;
 };
 
 const setDepts = (deptArray) => {
   departments = deptArray;
 };
 
+const getItems = () => {
+  return items;
+};
+
 module.exports = {
   getDepts,
   setDepts,
+  getItems,
+  setItems,
 };
