1349b7c41452eed449639b7af53b50f5a7148700
April Nichols <nicholsar@gmail.com>
2018-04-16 18:37:29 -0500

    items xhr

--- javascripts/dataGatekeeper.js
+++ javascripts/dataGatekeeper.js
@@ -1,19 +1,26 @@
 const callDepartments = require('./departments');
+const callItems = require('./items');
 const deptDom = require('./departmentDom');
 const data = require('./data');
 
-function whenDeptsLoad () {
+const whenDeptsLoad = function () {
   const deptData = JSON.parse(this.responseText).departments;
   data.setDepts(deptData);
   deptDom(deptData);
 };
 
-function ifDeptsFail () {
+const whenItemsLoad = function () {
+  const itemData = JSON.parse(this.responseText).items;
+  console.log(itemData);
+};
+
+const ifLoadFails = function () {
   console.error('Something is wrong.');
 };
 
 const initializer = () => {
-  callDepartments(whenDeptsLoad, ifDeptsFail);
+  callDepartments(whenDeptsLoad, ifLoadFails);
+  callItems(whenItemsLoad, ifLoadFails);
 };
 
 module.exports = {
---
+++ javascripts/items.js
@@ -0,0 +1,9 @@
+const callItems = (successFunction, errorFunction) => {
+  const myRequest = new XMLHttpRequest();
+  myRequest.addEventListener('load', successFunction);
+  myRequest.addEventListener('error', errorFunction);
+  myRequest.open('GET', '../db/items.json');
+  myRequest.send();
+};
+
+module.exports = callItems;
